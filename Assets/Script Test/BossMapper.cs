24623b322a6204e591f983494be517b11ef26356
Raphael Palmeri <Raphael Palmeri>
2016-06-15 17:34:13 +0200

    Instantiate Maps : OK (Need to move player and camera to the map above

---
+++ Assets/Script Test/BossMapper.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BossMapper : MonoBehaviour {
+
+    List<Object> BossMaps = new List<Object>();
+    int rand;
+
+    void Awake()
+    {
+        BossMaps.Add(Resources.Load("SalleBoss1"));
+        BossMaps.Add(Resources.Load("SalleBoss2"));
+        BossMaps.Add(Resources.Load("SalleBoss3"));
+    }
+
+    void Start()
+    {
+        rand = Random.Range(0, BossMaps.Count);
+        Instantiate(BossMaps[rand], transform.position, transform.rotation);
+    }
+}
---
+++ Assets/Script Test/Mapper.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Mapper : MonoBehaviour {
+
+    List<Object> Maps = new List<Object>();
+    int rand;
+
+    void Awake()
+    {
+        Maps.Add(Resources.Load("Salle 1"));
+        Maps.Add(Resources.Load("Salle 2"));
+        Maps.Add(Resources.Load("Salle 3"));
+        Maps.Add(Resources.Load("Salle 4"));
+        Maps.Add(Resources.Load("Salle 5"));
+        Maps.Add(Resources.Load("Salle 6"));
+        Maps.Add(Resources.Load("Salle 7"));
+        Maps.Add(Resources.Load("Salle 8"));
+        Maps.Add(Resources.Load("Salle 9"));
+        Maps.Add(Resources.Load("Salle 10"));
+        Maps.Add(Resources.Load("Salle 11"));
+        Maps.Add(Resources.Load("Salle 12"));
+        Maps.Add(Resources.Load("Salle 13"));
+        Maps.Add(Resources.Load("Salle 14"));
+        Maps.Add(Resources.Load("Salle 15"));
+    }
+
+    void Start()
+    {
+        rand = Random.Range(0, Maps.Count);
+        Instantiate(Maps[rand], transform.position, transform.rotation);
+    }
+}
